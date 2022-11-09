80840d7e201e0291626e026b821a37596d940615
Mostofa321 <hmshumon123@gmail.com>
2022-11-10 01:16:53 +0600

    created post api for sending data from client site to database

--- index.js
+++ index.js
@@ -21,7 +21,26 @@ app.get('/', (req, res) => {
 // mongodb crud operation
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nihba2p.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async () => {
+  try {
+    const foodCollection = client.db("foodsDb").collection("foodsCollection");
 
+    // send food to database 
+    app.post('/food', async (req, res) => {
+      const food = req.body;
+      const result = await foodCollection.insertOne(food);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.send(result);
+    });
+
+    
+ 
+  }
+  catch (err) {
+    console.log(err.name, err.message, err.stack)
+  }
+}
+run();
 
 
 app.listen(port, () => {
